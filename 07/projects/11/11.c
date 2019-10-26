c58252e0b5161a628002d58069f7e7262d502bc0
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2019-10-26 15:53:57 +0200

    Revert "Syncing a fork"

--- 07/projects/11/11.c
+++ 07/projects/11/11.c
@@ -5,18 +5,14 @@ int main(void) {
     char c, initial;
 
     printf("Enter a first and last name: ");
-    scanf(" %c", &initial);
     
-    while ((c = getchar()) != ' ')
-        ;
-
     while ((c = getchar()) == ' ')
         ;
-
-    do {
+    initial = c;
+    while ((c = getchar()) != ' ')
+        ;
+    while ((c = getchar()) != '\n')
         putchar(c);
-    } while ((c = getchar()) != '\n' && c != ' ');
-
     printf(", %c.\n", initial);
 
     return 0;
